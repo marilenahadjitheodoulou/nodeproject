e725bfa9d89562d32f60476730235b04fc8769fb
marilenahadjitheodoulou <it216102@hua.gr>
2021-02-13 14:32:23 +0200

    post commit

--- app.js
+++ app.js
@@ -2,62 +2,64 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('movies.sqlite');
 
 const express = require('express')
+const bodyParser = require('body-parser');
+
 const app = express()
-const cors = require('cors');
-app.use(cors());
 
 app.use(express.static('./public'))
 
-const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/movies/:movieId', async (req, res)=>{
-    const q = `select movieId,title,genres from movies where movieId = ${req.params.movieId}`;
-    try{
-        const results = await query(q);
-        res.json(results);
-    }catch(err){
-        console.error(err);
-        res.append('status', '500');
-        res.send(err);
-    }
-});
 
-app.get('/ratings/:userId', async (req, res)=>{
-    const q = `select * from ratings where userId = ${req.params.userId}`;
-    try{
-        const results = await query(q);
-        res.json(results);
-    }catch(err){
-        console.error(err);
-        res.append('status', '500');
-        res.send(err);
-    }
-});
-
-app.post('/view', function(req,res){
-    db.serialize(()=>{
-      db.each('SELECT * FROM movies WHERE movieId =?', [req.body.movieId], function(err,row){     //db.each() is only one which is funtioning while reading data from the DB
-        if(err){
-          res.send("Error encountered while displaying");
-          return console.error(err.message);
+app.get('/movies/:movieId', (req, res) => {
+    const q = `select * from movies where movieId = ${req.params.movieId}`;
+    db.all(q, (err, rows) => {
+        if (err) {
+            res.json({ 'message': 'failure', 'code': err });
+            console.error(err);
+            return;
         }
-        res.send(` ID: ${row.movieId},    Title: ${row.title}`);
-        console.log("Entry displayed successfully");
-      });
+        res.type('json').send(JSON.stringify(rows, null, 2) + '\n');
     });
 });
 
-app.listen(8080,function(){ 
+app.get('/ratings/:userId', async (req, res) => {
+    const q = `select * from ratings where userId = ${req.params.userId}`;
+    db.all(q, (err, rows) => {
+        if (err) {
+            res.json({ 'message': 'failure', 'code': err });
+            console.error(err);
+            return;
+        }
+        res.type('json').send(JSON.stringify(rows, null, 2) + '\n');
+    });
+});
+
+app.post('/view', (req, res) => {
+    var keyword = req.body.keyword;
+    const q = `select * from movies where title LIKE '%${req.body.keyword}%' `;
+    db.all(q, (err, rows) => {
+        if (err) {
+            res.json({ 'message': 'failure', 'code': err });
+            console.error(err);
+            return;
+        }
+        res.type('json').send(JSON.stringify(rows, null, 2) + '\n');
+
+    });
+});
+
+app.listen(8080, function () {
     console.log("App listening on port: 8080");
 });
 
 var request = require('request');
 
-function query(q){
-    return new Promise(function(resolve, reject){
-        db.all(q,(err,rows)=>{
-            if(err){
+function query(q) {
+    return new Promise(function (resolve, reject) {
+        db.all(q, (err, rows) => {
+            if (err) {
                 console.error(err);
                 reject(err);
                 return;
